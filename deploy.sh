f6405a58a108b0b112e33209923ea10b41e74ab9
liuwenshun <liuwenshun@u51.com>
2018-04-10 20:50:54 +0800

    feat: webhook test

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rm -rf package-lock.json && rm -rf node_modules/ && npm i && pm2 restart app && echo "服务启动成功"
+rm -rf package-lock.json && rm -rf node_modules/ && git pull && npm i && pm2 restart app && echo "服务启动成功"
 
